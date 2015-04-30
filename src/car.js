6b69551d61842d2859f74abbb295ff794c7b062c
MacBook <MacBook@MacBooks-MacBook-Pro.local>
2015-04-30 17:25:20 +0100

    fourth test passed

--- src/car.js
+++ src/car.js
@@ -1,10 +1,11 @@
-function Car(make, model, year, color, state, previousOwners){
+function Car(make, model, year, color, state, previousOwners, currentOwner){
   this.make = make;
   this.model = make;
   this.year = year;
   this.color = color;
   this.state = state;
   this.previousOwners = previousOwners;
+  this.currentOwner = currentOwner;
 }
 
 
